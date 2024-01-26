f64665f77db9caa24b6225ff582315471ae96b8f
Richard Kubel <Richard@LRichards-MacBook-Pro.local>
2024-01-26 08:49:05 -0800

    first commmit

---
+++ models.py
@@ -0,0 +1,135 @@
+"""Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+DEFAULT_IMAGE_URL = "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png"
+
+
+class User(db.Model):
+    """Site user."""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.Text, nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
+    image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)
+
+    @property
+    def full_name(self):
+        """Return full name of user."""
+
+        return f"{self.first_name} {self.last_name}"
+
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+
+    You should call this in your Flask app.
+    """
+
+    db.app = app
+    db.init_app(app)
+part-one/app.py
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///blogly"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'ihaveasecret'
+
+# Having the Debug Toolbar show redirects explicitly is often useful;
+# however, if you want to turn it off, you can uncomment this line:
+#
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+toolbar = DebugToolbarExtension(app)
+
+
+connect_db(app)
+db.create_all()
+
+
+@app.route('/')
+def root():
+    """Homepage redirects to list of users."""
+
+    return redirect("/users")
+
+
+##############################################################################
+# User route
+
+@app.route('/users')
+def users_index():
+    """Show a page with info on all users"""
+
+    users = User.query.order_by(User.last_name, User.first_name).all()
+    return render_template('users/index.html', users=users)
+
+
+@app.route('/users/new', methods=["GET"])
+def users_new_form():
+    """Show a form to create a new user"""
+
+    return render_template('users/new.html')
+
+
+@app.route("/users/new", methods=["POST"])
+def users_new():
+    """Handle form submission for creating a new user"""
+
+    new_user = User(
+        first_name=request.form['first_name'],
+        last_name=request.form['last_name'],
+        image_url=request.form['image_url'] or None)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect("/users")
+
+
+@app.route('/users/<int:user_id>')
+def users_show(user_id):
+    """Show a page with info on a specific user"""
+
+    user = User.query.get_or_404(user_id)
+    return render_template('users/show.html', user=user)
+
+
+@app.route('/users/<int:user_id>/edit')
+def users_edit(user_id):
+    """Show a form to edit an existing user"""
+
+    user = User.query.get_or_404(user_id)
+    return render_template('users/edit.html', user=user)
+
+
+@app.route('/users/<int:user_id>/edit', methods=["POST"])
+def users_update(user_id):
+    """Handle form submission for updating an existing user"""
+
+    user = User.query.get_or_404(user_id)
+    user.first_name = request.form['first_name']
+    user.last_name = request.form['last_name']
+    user.image_url = request.form['image_url']
+
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect("/users")
+
+
+@app.route('/users/<int:user_id>/delete', methods=["POST"])
+def users_destroy(user_id):
+    """Handle form submission for deleting an existing user"""
+
+    user = User.query.get_or_404(user_id)
+    db.session.delete(user)
+    db.session.commit()
+
+    return redirect("/users")
